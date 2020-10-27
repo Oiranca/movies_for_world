61c692bb616ef44328ef006ec4b39d46fa6d2378
Samuel Romero Arbelo <oiranca@gmail.com>
2020-10-27 17:02:42 +0000

    Logo and some components

---
+++ src/components/navbar/NavBar.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import logotype from '../../img/logo/logo.png'
+
+
+const NavBar = () => {
+
+  return (
+    <React.Fragment>
+      <header>
+        <img src={logotype} alt="Logotipo"/>
+      </header>
+
+
+    </React.Fragment>
+
+  )
+
+
+}
+
+export default NavBar;
