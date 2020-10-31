6eca29f2ebbb27a48268b0eeec1d0b7f4b3a092e
Samuel Romero Arbelo <oiranca@gmail.com>
2020-10-31 16:21:17 +0000

    Fix some problems with input placeholder

---
+++ src/components/container/Container.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import './Container.scss'
+import * as ReactBootstrap from "react-bootstrap";
+
+const Container=(props)=>{
+
+  return(
+    <main className='container-main'>Hola</main>
+  )
+
+
+}
+
+export default Container;
